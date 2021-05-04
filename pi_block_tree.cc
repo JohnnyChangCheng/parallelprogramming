07685907177b4b368f6311afe9cf8ebd657e803d
ChangCheng <37439139+JohnnyChangCheng@users.noreply.github.com>
2021-05-04 10:02:56 +0800

    Update pi_block_tree.cc

--- pi_block_tree.cc
+++ pi_block_tree.cc
@@ -4,43 +4,74 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 
-int main(int argc, char **argv) {
-  // --- DON'T TOUCH ---
-  MPI_Init(&argc, &argv);
-  double start_time = MPI_Wtime();
-  double pi_result;
-  long long int tosses = atoi(argv[1]);
-  int world_rank, world_size;
-  int pow_of_2 = 0;
-  // ---
-
-  // ---
-
-  // TODO: init MPI
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  // Calculate pow of 2
-  while (1) {
-    if ((1 << pow_of_2) == world_size) {
-      break;
-    }
-    ++pow_of_2;
-  }
-
-  // TODO: binary tree redunction
-
-  if (world_rank == 0) {
-    // TODO: PI result
-
+int main(int argc, char **argv)
+{
     // --- DON'T TOUCH ---
-    double end_time = MPI_Wtime();
-    printf("%lf\n", pi_result);
-    printf("MPI running time: %lf Seconds\n", end_time - start_time);
-    // ---
-  }
+    MPI_Init(&argc, &argv);
+    double start_time = MPI_Wtime();
+    double pi_result;
+    long long int tosses = atoi(argv[1]);
+    int world_rank, world_size;
+    long long int counter = 0;
 
-  MPI_Finalize();
-  return 0;
+    // TODO: init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    for (long long int i = 0; i < tosses; ++i)
+    {
+        double x = ((double)rand()) / RAND_MAX;
+        double y = ((double)rand()) / RAND_MAX;
+        double z = sqrt(x * x + y * y);
+        if (z <= 1)
+        {
+            counter++;
+        }
+    }
+    // Calculate pow of 2
+    int pow_of_2 = (int)(log(world_size) / log(2));
+
+    for (int i = 0; i < pow_of_2; ++i)
+    {
+        int last_idx = -1;
+        for (int j = 0; j < world_size; j += pow(2, i))
+        {
+            if (last_idx == -1)
+            {
+                last_idx = j;
+            }
+            else
+            {
+                if (last_idx == world_rank)
+                {
+                    long long int buffer = 0;
+                    MPI_Recv(&buffer, 1, MPI_LONG_LONG_INT,
+                             j, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    counter += buffer;
+                }
+                if (j == world_rank)
+                {
+                    MPI_Send(&counter, 1, MPI_LONG_LONG_INT, last_idx, 1, MPI_COMM_WORLD);
+                }
+                last_idx = -1;
+            }
+        }
+    }
+    // TODO: binary tree redunction
+
+    if (world_rank == 0)
+    {
+        // TODO: PI result
+        pi_result = (double)counter / (double)(tosses*world_size);
+        // --- DON'T TOUCH ---
+        double end_time = MPI_Wtime();
+        printf("%lf\n", pi_result);
+        printf("MPI running time: %lf Seconds\n", end_time - start_time);
+        // ---
+    }
+
+    MPI_Finalize();
+    return 0;
 }
